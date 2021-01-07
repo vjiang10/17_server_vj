371973bb01cdf5b41882b8ed622e88debda62f87
Vincent Jiang <vincentjiang2003@gmail.com>
2021-01-07 13:56:42 -0500

    added initial files

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+int main() {
+    
+}
---
+++ server.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    
+}
